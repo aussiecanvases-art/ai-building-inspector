244c2ea13977b538c342784b353f38102044935f
aussiecanvases-art <aussiecanvases@gmail.com>
2026-03-26 00:25:09 +1100

    Create initial App component with basic layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>AI Building Inspector</h1>
+      <p>App is working 🚀</p>
+    </div>
+  );
+}
+
+export default App;
